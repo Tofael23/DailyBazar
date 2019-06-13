a0ee3928ed4d87386ea8eed7323613e2cb8b6594
tofael ahmed <t.a.cse96@gmail.com>
2019-06-14 02:34:42 +0600

    Add Product & Category Business Entities

---
+++ DailyBazar.Database/ProjectContext.cs
@@ -0,0 +1,16 @@
+﻿using DailyBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyBazar.Database
+{
+    public class ProjectContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
